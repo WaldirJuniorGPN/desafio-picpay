9b9811a30921cfa34473569ca10df3acad9aa897
Waldir Júnior <128874158+WaldirJuniorGPN@users.noreply.github.com>
2025-03-28 01:32:53 +0000

    feat: adiciona exception personalizada referente a regras de negócio

---
+++ src/main/java/br/com/desafio_picpay/desafio_pickpay/domain/exception/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package br.com.desafio_picpay.desafio_pickpay.domain.exception;
+
+public class RegraDeNegocioException extends RuntimeException {
+
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
